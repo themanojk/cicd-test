d6a856c2861a04e0d98a37aac3de46091f2a7459
Manoj Kumar <themanojkum@gmail.com>
2021-04-12 16:35:52 +0530

    first draft

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+
+app.listen(3000, () => {
+    console.log('Listeninig onm 3000');
+})
+
